ba0e501cd54594e9b0b1c4006b7cfad02d862f20
samsulalamasif <samsulalamasif@gmail.com>
2023-04-10 00:42:57 +0600

    all json file created

---
+++ README.md
@@ -0,0 +1,9 @@
+### The Hire Agency
+
+Private Repo Link:
+
+## [https://github.com/Porgramming-Hero-web-course/b7a9-career-hub-samsulalamasif.git](https://github.com/Porgramming-Hero-web-course/b7a9-career-hub-samsulalamasif.git)
+
+Live Link:
+
+## []()
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
